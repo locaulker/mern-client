11dc477ee54665ed0810837f7abca79f78f1a63f
Lawrence <Caulker>
2020-09-13 14:14:43 -0400

    added signup form structure @11:46

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -6,10 +6,71 @@ import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css'
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    buttonText: 'Submit',
+  })
+
+  const { name, email, password, buttonText } = values
+
+  const handleChange = name => event => {
+    //
+  }
+
+  const clickSubmit = event => {
+    //
+  }
+
+  const signupForm = () => (
+    <form>
+      <div className='form-group'>
+        <label htmlFor='name' className='text-muted'>
+          Name
+        </label>
+        <input
+          onChange={handleChange('name')}
+          type='text'
+          className='form-control'
+        />
+      </div>
+
+      <div className='form-group'>
+        <label htmlFor='email' className='text-muted'>
+          eMail
+        </label>
+        <input
+          onChange={handleChange('email')}
+          type='email'
+          className='form-control'
+        />
+      </div>
+
+      <div className='form-group'>
+        <label htmlFor='' className='text-muted'>
+          Password
+        </label>
+        <input
+          onChange={handleChange('password')}
+          type='password'
+          className='form-control'
+        />
+      </div>
+
+      <div>
+        <button className='btn btn primary' onClick={clickSubmit}>
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  )
+
   return (
     <Layout>
       <ToastContainer />
       <h1>Sign Up</h1>
+      {signupForm()}
     </Layout>
   )
 }
